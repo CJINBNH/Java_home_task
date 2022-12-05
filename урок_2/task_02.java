c1386bae22448c32018d82ffe589f4097b2ac740
CJINBNH <alex_egorov_89@mail.ru>
2022-12-05 21:03:51 +0400

    Урок 2. Почему вы не можете не использовать API

--- урок_2/task_02.java
+++ урок_2/task_02.java
@@ -1,6 +1,53 @@
 // Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой
 // итерации запишите в лог-файл.
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class task_02 {
+    public static void main(String[] args) {
+        int [] workArray = { 19, 13, 15, 18, 17, 10, 14, 12, 11, 16 };
+        
+        arrayPrint(workArray);
+        System.out.println();
+        sortFunction (workArray);
+        arrayPrint(workArray);
+        System.out.println();
+    }
+
     
+    public static void arrayPrint (int[] array) {
+        for (int i = 0; i < array.length; i++){
+            System.out.print(array[i]);
+            if(i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+
+    public static void sortFunction (int[] array) {
+        boolean sort = false;
+        int temporary;
+        while(!sort) {
+            sort = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i+1]) {
+                    sort = false;
+                    temporary = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temporary;
+
+                    String str = Arrays.toString(array);
+                    try (FileWriter writer = new FileWriter("log_file_урок2_task_02.txt", true)) {
+                        writer.write(str);            
+                        writer.append('\n');
+                        writer.flush();
+                    } catch (IOException ex) {
+                        System.out.println(ex.getMessage());
+                    }
+                }
+            }
+        }
+    }
 }
--- урок_2/task_03.java
+++ урок_2/task_03.java
@@ -8,6 +8,46 @@
 // Студент Петрова получил 4 по предмету Информатика.
 // Студент Краснов получил 5 по предмету Физика.
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class task_03 {
-    
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("string_урок_2_task_03.txt"));
+        String data;
+        StringBuilder builderStr1 = new StringBuilder();
+        while ((data = bufferedReader.readLine()) != null) {
+            System.out.println(data);
+            data = data.substring(1, data.length() - 1);
+            data = data.replaceAll("\\{", "");
+            data = data.replaceAll("\\}", "");
+            String[] dn = data.split(", ");
+            
+            for (int i = 0; i < dn.length; i++) {
+                String[] dn1 = dn[i].split(",");
+                
+                for (int j = 0; j < dn1.length; j++) {
+                    String[] dn2 = dn1[j].split(":");
+                    String key = dn2[0];
+                    key = key.substring(1, key.length() - 1);
+                    String value = dn2[1];
+                    value = value.substring(1, value.length() - 1);
+                    builderStr1.append(value);
+                    builderStr1.append(" ");
+                }
+            }
+        }
+        String[] strArray = builderStr1.toString().split(" ");
+        for (int i = 0; i < strArray.length; i += 3) {
+            System.out.println("Студент " + strArray[i] + " получил " + strArray[i + 1]
+                                + " по предмету " + strArray[i + 2] + ".");
+        }
+        try {
+            if (bufferedReader != null) {
+                bufferedReader.close();
+            }
+        } catch (Exception e) {
+            System.out.println("Ошибка чтения файла");
+        }
+    }
 }
